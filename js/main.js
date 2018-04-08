1dbd09823e7d748c920e0f944677aec8368030a1
YanYuanYuan <yanyuanyuan@4399inc.com>
2018-04-08 10:48:16 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by yanyuanyuan/媛媛 on 2018/4/8.
+ * Description:
+ */
+function init() {
+    // ...
+}
+//渲染器
+var renderer = new THREE.WebGLRenderer({
+    canvas: document.getElementById('mainCanvas')
+});
